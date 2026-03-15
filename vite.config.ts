088954fbded207fbdaae946bd450ea452792d054
Methul Jayasuriya <methullakvindu5@gmail.com>
2026-03-15 23:19:20 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-})
+});
